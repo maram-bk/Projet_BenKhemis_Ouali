908368ca66c49179dc31b990fc31c7d9fed826ff
maram <benkhemismaram150@gmail.com>
2025-11-12 22:13:08 +0100

    update et delete

--- src/app/services/site-service.ts
+++ src/app/services/site-service.ts
@@ -4,19 +4,25 @@ import { Observable } from 'rxjs';
 import { SiteArcheologique } from '../models/site-archeologique';
 
 
-const API_URL="http://localhost:3000/sites";
+const API_URL = "http://localhost:3000/sites";
 @Injectable({
   providedIn: 'root'
 })
 export class SiteService {
-  private readonly http:HttpClient=inject (HttpClient);
-  getAllSite():Observable<SiteArcheologique []>{
-    return this.http.get<SiteArcheologique []>(API_URL);
+  private readonly http: HttpClient = inject(HttpClient);
+  getAllSite(): Observable<SiteArcheologique[]> {
+    return this.http.get<SiteArcheologique[]>(API_URL);
   }
-  getSiteById(id:string):Observable<SiteArcheologique>{
+  getSiteById(id: string): Observable<SiteArcheologique> {
     return this.http.get<SiteArcheologique>(`${API_URL}/${id}`);
   }
-  addSite(s:SiteArcheologique):Observable<SiteArcheologique>{
-    return this.http.post<SiteArcheologique>(API_URL,s);
+  addSite(s: SiteArcheologique): Observable<SiteArcheologique> {
+    return this.http.post<SiteArcheologique>(API_URL, s);
+  }
+  updateSiteById(id: string, site: SiteArcheologique): Observable<SiteArcheologique> {
+    return this.http.put<SiteArcheologique>(`${API_URL}/${id}`, site);
+  }
+  deleteSiteById(id: string): Observable<void> {
+    return this.http.delete<void>(`${API_URL}/${id}`);
   }
 }
